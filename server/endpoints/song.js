4b8f08c7b3a644c4aef126d4e1b3be52918c2022
hiordanova <hiordanova@securitycompass.com>
2025-01-10 12:04:48 -0500

    WIP

--- server/endpoints/song.js
+++ server/endpoints/song.js
@@ -46,10 +46,89 @@ const getWinningSongs = async (req, res, client) => {
   res.json({ winningSongs });
 };
 
+const _checkDuplicates = async (client, gameId, songData, scenarios) => {
+  const players = await client.lRange(`sns:${gameId}:players`, 0, -1);
+  const submissions = await Promise.all(
+    players.map(
+      async (id) =>
+        await client.lRange(`sns:${gameId}:player:${id}:songs`, 0, -1)
+    )
+  );
+
+  const duplicates = [];
+  scenarios.forEach((scenario, i) => {
+    const song = songData.find((s) => s.scenario === scenario).song;
+    const submittedSongs = submissions
+      .filter((submission) => submission.length)
+      .map((submission) => submission[i]);
+
+    if (submittedSongs.includes(song)) {
+      duplicates.push(scenario);
+    }
+  });
+
+  return duplicates;
+};
+
+const _getYouTubeVideoDetails = async (url) => {
+  const videoId = videoUrl.split("v=")[1]?.split("&")[0];
+  if (!videoId) throw new Error("Invalid YouTube video URL");
+
+  const apiKey = process.env.YOUTUBE_API_KEY;
+  try {
+    const response = await axios.get(
+      `https://www.googleapis.com/youtube/v3/videos`,
+      {
+        params: {
+          part: "snippet",
+          id: videoId,
+          key: apiKey,
+        },
+      }
+    );
+
+    if (response.data.items.length === 0) throw new Error("Video not found");
+
+    const videoDetails = response.data.items[0].snippet;
+    return {
+      videoId,
+      title: videoDetails.title,
+      description: videoDetails.description,
+    };
+  } catch (error) {
+    console.error("YouTube API Error:", error.message);
+    throw new Error("Error failed to fetch video details");
+  }
+};
+
+const _saveSongs = async (client, gameId, playerId, songData) => {
+  const errors = [];
+  const songDetails = [];
+  for (let song of songData) {
+    try {
+      const videoDetails = await _getYouTubeVideoDetails(song.url);
+      songDetails.push({ scenario: song.scenario, ...videoDetails });
+    } catch (error) {
+      errors.push({ scenario: song.scenario, error: error.message });
+      continue;
+    }
+  }
+  if (errors.length) return { errors };
+
+  for (let song of songDetails) {
+    const key = `sns:${gameId}:player:${playerId}:song:${song.videoId}`;
+    await client.hSet(key, song);
+  }
+
+  await client.rPush(
+    `sns:${gameId}:player:${playerId}:songs`,
+    songDetails.map((s) => s.videoId)
+  );
+};
+
 const submitSongs = async (req, res, client, sockets) => {
   const gameId = req.params.gameId;
-  const playerId = req.body.playerId;
-  const songData = req.body.songs;
+  const { playerId, songData } = req.body;
   const songsKey = `sns:${gameId}:player:${playerId}:songs`;
 
   const game = await client.hGetAll(`sns:${gameId}`);
@@ -60,29 +139,13 @@ const submitSongs = async (req, res, client, sockets) => {
     return;
   }
 
-  const players = await client.lRange(`sns:${gameId}:players`, 0, -1);
-  const submissions = await Promise.all(
-    players.map(
-      async (id) =>
-        await client.lRange(`sns:${gameId}:player:${id}:songs`, 0, -1)
-    )
-  );
-
   const scenarios = await client.lRange(`sns:${gameId}:scenarios`, 0, -1);
-
-  const duplicates = [];
-  const songs = scenarios.map((scenario, i) => {
-    const song = songData.find((s) => s.scenario === scenario).song;
-    const submittedSongs = submissions
-      .filter((submission) => submission.length)
-      .map((submission) => submission[i]);
-
-    if (submittedSongs.includes(song)) {
-      duplicates.push(scenario);
-    }
-    return song;
-  });
-
+  const duplicates = await _checkDuplicates(
+    client,
+    gameId,
+    songData,
+    scenarios
+  );
   if (duplicates.length) {
     const message = `The songs chosen for the following scenarios have already been selected: ${duplicates.join(
       ", "
@@ -92,7 +155,7 @@ const submitSongs = async (req, res, client, sockets) => {
     return;
   }
 
-  await client.rPush(songsKey, songs);
+  await _saveSongs(client, gameId, playerId, songData);
 
   const modSocket = sockets[gameId][game.mod];
   if (modSocket) {
