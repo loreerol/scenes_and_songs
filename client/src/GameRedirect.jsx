6e95c6bcd7dbcf34630328c0b9bfa1985b0e7780
hiordanova <hiordanova@securitycompass.com>
2025-01-10 12:05:32 -0500

    Minor fixes for console errors

--- client/src/GameRedirect.jsx
+++ client/src/GameRedirect.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { GameContext } from "./GameProvider";
@@ -7,17 +7,21 @@ const GameRedirect = () => {
   const { gameId, gameState } = useContext(GameContext);
   const navigate = useNavigate();
 
-  if (["lobby", "song-selection"].includes(gameState)) {
-    navigate(`/game/${gameId}/set-up`);
-  } else if (gameState === "music-phase") {
-    navigate(`/game/${gameId}/music`);
-  } else if (["voting-phase", "voting-phase-results"].includes(gameState)) {
-    navigate(`/game/${gameId}/vote`);
-  } else if (["guessing-phase", "guessing-phase-results"].includes(gameState)) {
-    navigate(`/game/${gameId}/guess`);
-  } else if (["round-results", "game-over"].includes(gameState)) {
-    navigate(`/game/${gameId}/results`);
-  }
+  useEffect(() => {
+    if (["lobby", "song-selection"].includes(gameState)) {
+      navigate(`/game/${gameId}/set-up`);
+    } else if (gameState === "music-phase") {
+      navigate(`/game/${gameId}/music`);
+    } else if (["voting-phase", "voting-phase-results"].includes(gameState)) {
+      navigate(`/game/${gameId}/vote`);
+    } else if (
+      ["guessing-phase", "guessing-phase-results"].includes(gameState)
+    ) {
+      navigate(`/game/${gameId}/guess`);
+    } else if (["round-results", "game-over"].includes(gameState)) {
+      navigate(`/game/${gameId}/results`);
+    }
+  }, [gameId, gameState, navigate]);
 
   return (
     <div>
