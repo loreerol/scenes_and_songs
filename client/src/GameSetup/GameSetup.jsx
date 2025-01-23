811b15410235b89196cd6faccd0092cc7c5eb8d1
lhill <lhill@securitycompass.com>
2025-01-22 20:52:14 -0500

    simplified layout component reuse

--- client/src/GameSetup/GameSetup.jsx
+++ client/src/GameSetup/GameSetup.jsx
@@ -1,42 +1,32 @@
 import React, { useContext } from "react";
-import Layout from "../Layout";
 import { GameContext } from "../GameProvider";
 import ModScenarios from "./ModScenarios";
 import PlayerJoin from "./PlayerJoin";
 import SongSubmition from "./SongSubmition";
 
 const GameSetup = () => {
-  const { gameId, playerId, playerName, isMod, loading, gameState } =
+  const { gameId, playerId, isMod, loading, gameState } =
     useContext(GameContext);
-  console.info("GameSetup", {
-    gameId,
-    playerId,
-    playerName,
-    isMod,
-    loading,
-    gameState,
-  });
 
-  let content = null;
+  const renderContent = () => {
+    if (loading) return <p>Loading...</p>;
 
-  if (loading) {
-    content = <p>Loading...</p>;
-  } else if (!playerId) {
-    content = <PlayerJoin gameId={gameId} />;
-  } else if (isMod) {
-    content = <ModScenarios />;
-  } else {
+    if (!playerId) return <PlayerJoin gameId={gameId} />;
+
+    if (isMod) return <ModScenarios />;
+
     if (gameState === "lobby") {
-      content = (
+      return (
         <p className="text-xl text-yellow-200 text-center">
           🎭 Please wait for the moderator to create the scenarios...
         </p>
       );
-    } else if (gameState === "song-selection") {
-      content = <SongSubmition />;
     }
-  }
-  return <Layout>{content}</Layout>;
+
+    if (gameState === "song-selection") return <SongSubmition />;
+  };
+
+  return <>{renderContent()}</>;
 };
 
 export default GameSetup;
