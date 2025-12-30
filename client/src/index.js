29af19d93999fafcdad2516faf0c69e428b1c707
Loreerol <lorehill.hello@gmail.com>
2025-12-30 14:26:21 +0100

    global error boundary

--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,7 @@ import MusicPhase from "./components/MusicPhase";
 import VotingPhase from "./components/VotingPhase";
 import GuessingPhase from "./components/GuessingPhase";
 import Results from "./components/Results";
+import AppErrorBoundary from "./components/ErrorBoundary/AppErrorBoundary";
 
 export const queryClient = new QueryClient();
 
@@ -28,17 +29,19 @@ root.render(
   <QueryClientProvider client={queryClient}>
     <CookiesProvider defaultSetOptions={{ path: "/" }}>
       <BrowserRouter>
-        <GameProvider>
-          <Routes>
-            <Route path="/" element={<CreateGame />} />
-            <Route path="/game/:id" element={<GameRedirect />} />
-            <Route path="/game/:id/set-up" element={<GameSetup />} />
-            <Route path="/game/:id/music" element={<MusicPhase />} />
-            <Route path="/game/:id/vote" element={<VotingPhase />} />
-            <Route path="/game/:id/guess" element={<GuessingPhase />} />
-            <Route path="/game/:id/results" element={<Results />} />
-          </Routes>
-        </GameProvider>
+        <AppErrorBoundary>
+          <GameProvider>
+            <Routes>
+              <Route path="/" element={<CreateGame />} />
+              <Route path="/game/:id" element={<GameRedirect />} />
+              <Route path="/game/:id/set-up" element={<GameSetup />} />
+              <Route path="/game/:id/music" element={<MusicPhase />} />
+              <Route path="/game/:id/vote" element={<VotingPhase />} />
+              <Route path="/game/:id/guess" element={<GuessingPhase />} />
+              <Route path="/game/:id/results" element={<Results />} />
+            </Routes>
+          </GameProvider>
+        </AppErrorBoundary>
       </BrowserRouter>
     </CookiesProvider>
     <ReactQueryDevtools initialIsOpen={false} />
